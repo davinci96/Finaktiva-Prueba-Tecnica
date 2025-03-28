31d9c23400dc81677b29b570ba0eb6079fb48944
davinci96 <davis8santi@gmail.com>
2025-03-28 11:05:00 -0500

    ADD

--- infra/modules/vpc_stack.py
+++ infra/modules/vpc_stack.py
@@ -2,6 +2,7 @@ import os
 from aws_cdk import Stack
 from aws_cdk import aws_ec2 as ec2
 from constructs import Construct
+from aws_cdk import aws_logs as logs
 
 class VpcStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
